cfd66ee533e9e8ebf8ea5a80c4945396855f74a1
fadi <fadi.m.marouf@gmail.com>
2022-11-10 19:44:28 +0100

    orgenising before adding login route

--- server.js
+++ server.js
@@ -20,15 +20,7 @@ const { addAbortSignal } = require("stream");
 const cookieSession = require("cookie-session");
 app.use(cookieParser());
 const { PORT } = process.env;
-//console.log(createProfile('fadi', 'marouf'));
 
-// const countUsers=(req,res,next)=>{
-//     counter++;
-//     console.log(counter)
-
-//     next()
-// }
-//app.use(countUsers);
 
 app.use(
     require("cookie-session")({
@@ -38,6 +30,9 @@ app.use(
     })
 );
 
+
+
+// home route (registering)
 app.get("/", (req, res) => {
     const cookie = req.cookies;
 
@@ -46,17 +41,11 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    ///if (req.cookies.value)
-    //    { //console.log(req.cookies);
-    //     return
-    //     }
-
     let { firstName, lastName, email, password, signature } = req.body;
-
     if (!firstName && !lastName && !signature) {
         res.redirect("/");
     } // add partial to tell the user he/she to fill the fields?????
-    // check if userId exist scip the register page to the login page
+    // check if userId exist skip the register page to the login page
     const created_at = new Date();
     createUser({ firstName, lastName, email, password, created_at }).then(
         (user) => {
@@ -65,16 +54,14 @@ app.post("/", (req, res) => {
             req.session.userId = user.id;
             req.session.email = user.email;
             req.session.created_at = user.created_at;
-
             res.redirect("/sign");
         }
     );
 });
 
+//signing petition route after regestering
 app.get("/sign", (req, res) => {
-    //const cookie = req.cookies;
-
-    //if (cookie.session) res.redirect("/thanks");
+    
     res.render("sign");
 });
 app.post("/sign", (req, res) => {
@@ -84,12 +71,11 @@ app.post("/sign", (req, res) => {
     console.log("reqBody",req.body.signature);
     createSignatures({ userId, signature }).then(
         signature
-
-        //console.log("signatureData", signature)
     );
     res.redirect("/thanks");
 });
 
+// the Thanks route(after signing the petition)
 app.get("/thanks", (req, res) => {
     const { firstName, lastName, userId, signature } = req.session;
     
@@ -107,6 +93,24 @@ app.get("/signers", (req, res) => {
     getProfiles().then((signers) => {
         res.render("signers", { signers });
     });
+});
+
+
+
+// login route
+app.get('/login', (req,res)=>{
+    //let { firstName, lastName, email, password, signature } = req.body;
+})
+app.post("/sign", (req, res) => {
+    userId = req.session.userId;
+
+    let { signature } = req.body;
+    console.log("reqBody", req.body.signature);
+    createSignatures({ userId, signature }).then(
+        signature
+
+    );
+    res.redirect("/thanks");
 });
 
 app.listen(PORT, () => {
